58f0f78352c8b70848e57fdfe78d53021952fb2a
Alexander GQ Gerasiov <gq@cs.msu.su>
2014-07-26 01:53:16 +0400

    Convert partan to modifier tool for HTC One SV.
    Signed-off-by: Alexander GQ Gerasiov <gq@cs.msu.su>

--- partan.c
+++ partan.c
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <linux/fs.h>
 
 void usage(const char *name)
 {
@@ -96,25 +98,6 @@ uint32_t analize_entry(struct part_entry *entry, int e_num, int p_num, uint32_t 
 //p_num - number of partition
 //offset - 0 for MBR, EBR offset for extented partition, first EBR offset for next EBR link.
 {
-	if (p_num)
-		printf("------ Entry %d (partition %d) ------\n", e_num, p_num);
-	else
-		printf("------ Entry %d (next EBR) ------\n", e_num);
-
-	printf("Status: 0x%.2x\t\t", entry->status);
-	printf("Type: 0x%.2x\n", entry->type);
-
-	printf("CHS begin: 0x%.2x%.2x%.2x\t", entry->chs_begin[0], entry->chs_begin[1], entry->chs_begin[2]); 
-	printf("end: 0x%.2x%.2x%.2x\n", entry->chs_end[0], entry->chs_end[1], entry->chs_end[2]); 
-
-	printf("LBA begin: 0x%.8x (%u)\tsize: 0x%.8x (%u)\tend*: %.8x (%u)\n",
-			entry->lba_begin?entry->lba_begin + offset:0,
-			entry->lba_begin?entry->lba_begin + offset:0,
-			entry->lba_size, entry->lba_size,
-			entry->lba_size?entry->lba_begin + entry->lba_size - 1 + offset:0,
-			entry->lba_size?entry->lba_begin + entry->lba_size - 1 + offset:0
-			);
-
 	if (entry->type == 0x05 || entry->type == 0x0f) {
 		if (entry->lba_begin == 0)
 			printf("WARNING: type == 0x05 | 0x0f (Extended), but LBA begin = 0\n");
@@ -128,11 +111,6 @@ uint32_t analize_block(struct disk_block *block, uint32_t ebr, uint32_t f_ebr, i
 // f_ebr - 0 for MBR, first EBR offset for EBR
 // p_num - counter for partition
 {
-	if (!ebr)
-		printf("======= MBR =======\n");
-	else
-		printf("======= EBR (%u) =======\n", ebr);
-
 	uint32_t next_ebr = 0;
 	if (!ebr) { // MBR: all 4 entries contains partition.
 		for (unsigned int i = 0; i < sizeof(block->entry) / sizeof(block->entry[0]); i++) {
@@ -142,11 +120,120 @@ uint32_t analize_block(struct disk_block *block, uint32_t ebr, uint32_t f_ebr, i
 			if (n)
 				next_ebr = n;
 		}
-	} else { // EBR
-		// First entry contain partition with offset from current EBR
-		analize_entry(&block->entry[0], 1, (*p_num)++, ebr);
+	} else if (*p_num < 35){ // EBR
+		(*p_num)++;
 		// Second entry contains next EBR with offset from the first EBR
 		next_ebr = analize_entry(&block->entry[1], 2, 0, f_ebr);
+	} else { //*p_num
+		unsigned int ebr35 = ebr;
+#define CHECK_OLD
+#define MODIFY
+//#define DUMP_OLD
+
+		struct disk_block block35 = read_block(ebr35);
+
+#ifdef CHECK_OLD
+		if(!(
+			f_ebr == 262111 &&
+			ebr == 4030463 &&
+			block35.entry[0].status == 0 &&
+			block35.entry[0].type == 0x83 &&
+			block35.entry[0].lba_begin == 1 &&
+			block35.entry[0].lba_size == 2424831 &&
+			block35.entry[1].status == 0 &&
+			block35.entry[1].type == 5 &&
+			block35.entry[1].lba_begin == 6193184 &&
+			block35.entry[1].lba_size == 8699889 &&
+			block35.magic1 == MAGIC1 &&
+			block35.magic2 == MAGIC2)
+			) {
+			fprintf(stderr, "FATAL: ebr %d (partition 35) values are different from expexted ones.\n", ebr);
+			exit(1);
+		}
+#endif
+#ifdef DUMP_OLD
+		int s_fd = open("./erb35", O_CREAT|O_TRUNC|O_WRONLY);
+		if (s_fd == -1) {
+			fprintf(stderr, "Failed to open %s: %s\n", "./erb35", strerror(errno));
+			exit(1);
+		}
+		int res = write(s_fd, &block35, sizeof(block35));
+		close(s_fd);
+		if (res != sizeof(block35)) {
+			fprintf(stderr, "Failed to write to %s: %s\n", "./erb35", strerror(errno));
+			exit(1);
+		}
+#endif
+		unsigned int ebr36 = f_ebr + block35.entry[1].lba_begin;
+		struct disk_block block36 = read_block(ebr36);
+
+#ifdef CHECK_OLD
+		if(!(
+			ebr36 == 6455295 &&
+			block36.entry[0].status == 0 &&
+			block36.entry[0].type == 0xc &&
+			block36.entry[0].lba_begin == 1 &&
+			block36.entry[0].lba_size == 8699888 &&
+			block36.entry[1].status == 0 &&
+			block36.entry[1].type == 5 &&
+			block36.entry[1].lba_begin == 0 &&
+			block36.entry[1].lba_size == 0 &&
+			block36.magic1 == MAGIC1 &&
+			block36.magic2 == MAGIC2)
+			) {
+			fprintf(stderr, "FATAL: ebr %d (partition 36) values are different from expexted ones.\n", ebr36);
+			exit(1);
+		}
+#endif
+#ifdef DUMP_OLD
+		s_fd = open("./erb36", O_CREAT|O_TRUNC|O_WRONLY);
+		if (s_fd == -1) {
+			fprintf(stderr, "Failed to open %s: %s\n", "./erb36", strerror(errno));
+			exit(1);
+		}
+		res = write(s_fd, &block36, sizeof(block36));
+		close(s_fd);
+		if (res != sizeof(block36)) {
+			fprintf(stderr, "Failed to write to %s: %s\n", "./erb36", strerror(errno));
+			exit(1);
+		}
+#endif
+
+#define NEW_FAT_SIZE_MB 1024
+#define NEW_FAT_SIZE    (NEW_FAT_SIZE_MB * 1024 * 2)
+
+#ifdef MODIFY
+		unsigned int diff = block36.entry[0].lba_size - NEW_FAT_SIZE;
+
+		ebr36 = ebr36 + diff;
+
+		block35.entry[0].lba_size += diff;
+		block35.entry[1].lba_begin += diff;
+		block35.entry[1].lba_size -= diff;
+
+		block36.entry[0].lba_size = NEW_FAT_SIZE;
+//		block36.entry[1].type = 0;
+
+		write_block(ebr35, &block35);
+		write_block(ebr36, &block36);
+
+		//syncfs(dev_fd);
+		int iores = ioctl(dev_fd, BLKFLSBUF, NULL);
+		if (iores == -1) {
+			fprintf(stderr, "ioctl to sync device has failed: %s.\n", strerror(errno));
+//			      exit(3);
+		}
+
+		iores = ioctl(dev_fd, BLKRRPART, NULL);
+		printf("Partitions are resized.\n");
+		if (iores == -1) {
+			fprintf(stderr,"ioctl to re-read partition table has failed: %s.\n", strerror(errno));
+//			      exit(3);
+		}
+#else
+		printf("Modification disabled.\n");
+#endif
+		exit(0);
 	}
 	
 	if (block->magic1 != MAGIC1 || block->magic2 != MAGIC2) {
@@ -178,7 +265,7 @@ int main(int argc, const char** argv)
 
 	int p_num = 1;
 
-	dev_fd = open(file, O_RDONLY);
+	dev_fd = open(file, O_RDWR);
 	if (dev_fd < 0) {
 		printf("Failed to open %s: %s", file, strerror(errno));
 		return -1;
